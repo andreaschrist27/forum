454bd34bee50c59de91136619fdf1b9e09c97fd9
vagrant <vagrant@homestead>
2018-12-13 15:53:44 +0000

    clean up testing untuk reply

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/threads','ThreadsController@index');
-Route::get('/threads/{thread}','ThreadsController@show');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/threads','ThreadsController@index');
+Route::get('/threads/{thread}','ThreadsController@show');
+Route::post('/threads/{thread}/replies','RepliesController@store');

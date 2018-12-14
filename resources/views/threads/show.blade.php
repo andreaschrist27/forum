66c51bbcc248977af1c5b6e29b8667049cc31254
vagrant <vagrant@homestead>
2018-12-14 07:30:56 +0000

    buat reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -16,14 +16,27 @@
             </div>
         </div>
     </div>
-      <div class="row justify-content-center mt-2">
-        <div class="col-md-8">
-            <div class="card">
-               @foreach($thread->replies as $reply)
+    <div class="row justify-content-center ">
+   
+                @foreach($thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
+    </div>
+    @if(auth()->check())
+    <div class="row justify-content-center mt-2">
+        <div class="col-md-8">
+            <form method="POST" action="{{$thread->path().'/replies'}}">
+                @csrf
+                <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" placeholder="Say something ?" rows="5"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-default">Post</button>
+            </form>
         </div>
     </div>
+    @else
+        <p class="text-center">Please <a href="{{route('login')}}">Sign in </a>to participate in this discussion</p>
+    @endif
 </div>
 @endsection

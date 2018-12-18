ccc38e31896240c715ad678619b016990c373f44
vagrant <vagrant@homestead>
2018-12-18 03:01:48 +0000

    test validation session has errors

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -14,6 +14,9 @@ class RepliesController extends Controller
     
     public function store($chanelId,Thread $thread)
     {
+        $this->validate(request(),[
+            'body' => 'required'
+        ]);
         $thread->addReply([
             'body' =>request('body'),
             'user_id' => auth()->id()
